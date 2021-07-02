f432b84b919935f9df85f75fae906a1a8c9bb8e8
Dylan Mullins <dylanemullins92@gmail.com>
2021-07-02 13:39:51 -0500

    Added functionality to add tracks to a playlist

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,6 +5,24 @@ import { Playlist } from '../Playlist/Playlist';
 import React from 'react';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: [],
+      playlistName: 'Default Name',
+      playlistTracks: []
+    }
+    this.addTrack = this.addTrack.bind(this);
+  }
+
+  addTrack(track) {
+    if (this.state.playlistTracks.find(savedTrack => savedTrack.id === track.id)) {
+      return;
+    } else {      
+      this.setState({playlistTracks: this.state.playlistTracks.push(track)});
+    }
+  }
+
   render() {
     return (
       <div>
@@ -12,8 +30,8 @@ class App extends React.Component {
       <div className="App">
           <SearchBar />
         <div className="App-playlist">
-          <SearchResults />
-          <Playlist />
+          <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack}/>
+          <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks}/>
         </div>
       </div>
     </div>
--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import './Tract.css';
+import './Track.css';
 
-export class Tract extends React.Component {
+export class Track extends React.Component {
+    renderAction() {
+      if (this.props.isRemoval === true) {
+        return <button className="Track-action">-</button>
+      } else {
+        return <button className="Track-action">+</button>
+      }
+    }
+
     render() {
         return (
             <div className="Track">
               <div className="Track-information">
-                <h3>Default</h3>
-                <p></p>
+                <h3>{this.props.track.name}</h3>
+                <p>{this.props.track.artist}</p><p>{this.props.track.album}</p>
               </div>
               <button className="Track-action"></button>
             </div>
